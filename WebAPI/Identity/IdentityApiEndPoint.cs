ab6f535166f6e42aabef12ac9b168d668c08211d
Manvith <lobo.manoj@outlook.com>
2024-05-17 07:34:11 +0400

    generatetoaknendpoint added

---
+++ WebAPI/Identity/IdentityApiEndPoint.cs
@@ -0,0 +1,49 @@
+﻿using BussinessLogic;
+using DataAccessLayer.Model;
+using Domain;
+using Domain.Request;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Middlewares;
+
+
+
+
+namespace WebAPI.Identity;
+
+internal class IdentityApiEndPoint : IEndPointDefinition
+{
+
+
+    public void RegisterEndPoints(WebApplication application)
+    {
+        var IdentityGroup = application.MapGroup("identity/oauth2/v2.0/token");
+        IdentityAPI(IdentityGroup);
+
+    }
+
+    private static RouteGroupBuilder IdentityAPI(RouteGroupBuilder IdentityGroup)
+    {
+
+        IdentityGroup.MapPost("/", GenerateTokenAsync).Produces<Ok>().Produces<UnauthorizedHttpResult>().Produces<BadRequest>();
+
+        return IdentityGroup;
+
+    }
+
+
+
+    private static async Task<IResult?> GenerateTokenAsync([FromBody] ServiceRequest<PaymentModel> request)
+    {
+
+        //var response = await new PaymentBL().InsertPayment(new RequestModel<PaymentModel> { RequestObject = request.RequestObject});
+        //return ReturnResultBaseClass.returnResult(response);
+    }
+
+
+
+}
+
