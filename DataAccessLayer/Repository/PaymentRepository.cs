f95344a6784a524b5fa8b66e46237fa47844a032
Manoj Lobo <manoj.lobo@outlook.com>
2024-05-25 23:49:12 +0400

    bug fixes

--- DataAccessLayer/Repository/PaymentRepository.cs
+++ DataAccessLayer/Repository/PaymentRepository.cs
@@ -83,12 +83,14 @@ public class PaymentRepository : IPayment
         }
     }
 
-  async Task<ServiceResponse<string>> getOperaPaymentType(string AdeyanPaymentType)
+  async Task<ServiceResponse<string>> IPayment.getOperaPaymentType(string AdeyanPaymentType)
     {
         var OperaPaymentType = await new DapperHelper(config!._connectionString!).getOperaPaymentType(AdeyanPaymentType);
         return await new ServiceResult().GetServiceResponseAsync<string>(OperaPaymentType, ApplicationGenericConstants.SUCCESS, ApiResponseCodes.SUCCESS, 200, null);
 
     }
+
+   
 
 
     /*async Task<ServiceResponse<Domain.Responses.PaymentResponse>> IPayment.CapturePayment(Domain.Response.RequestModel<Domain.Response.PaymentRequest> request)
